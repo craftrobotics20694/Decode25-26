00fc46e87a20cf30373465cf91f16b62dc1b326d
craftrobotics20694 <craftrobotics20694@gmail.com>
2025-11-05 10:06:35 -0500

    mecanumManip

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Decode/manual/mecanumManip.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.Decode.manual;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import org.firstinspires.ftc.teamcode.Decode.mecanumConstants;
+
+public class mecanumManip extends OpMode{
+    DcMotorEx leftFront, leftBack, rightFront, rightBack;
+
+    @Override
+    public void init(){
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        leftFront.setDirection(mecanumConstants.mecanumConstants.leftFrontMotorDirection);
+
+        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
+        leftBack.setDirection(mecanumConstants.mecanumConstants.leftRearMotorDirection);
+
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        rightFront.setDirection(mecanumConstants.mecanumConstants.rightFrontMotorDirection);
+
+        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
+        rightBack.setDirection(mecanumConstants.mecanumConstants.rightRearMotorDirection);
+    }
+
+    @Override
+    public void loop(){
+        leftFront.setPower(-gamepad1.left_stick_y);
+        leftBack.setPower(-gamepad1.left_stick_y);
+        rightFront.setPower(-gamepad1.left_stick_y);
+        rightBack.setPower(-gamepad1.left_stick_y);
+    }
+}
